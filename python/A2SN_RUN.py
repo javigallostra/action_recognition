1006f8243721e3c4db1871ad0dcf3686cc1028c8
javigallostra <jgallostra@gmail.com>
2019-06-15 14:03:50 +0200

    New classes for running and plotting A2SN pools

--- python/A2SN_RUN.py
+++ python/A2SN_RUN.py
@@ -9,7 +9,6 @@ update the node values with occurred events
 class A2SN_RUN(A2SN_BASE):
 
     decay_factor = 0.95
-    tick = 0.1
 
     """
     Call A2SN_BASE init function, load graph data from file
@@ -28,39 +27,20 @@ class A2SN_RUN(A2SN_BASE):
         self.max_value = 0
         self.ti = 0
         self.active_events = []
-        self.update_thread = 0
-        self.stop_thread = False
         self.thread_lock = threading.Lock()
 
     """
     Update the active events with a new world input
     """
     def update_events(self, events = []):
-        self.update_events_thread = threading.Thread(target=self.__update_events,args=(events,),daemon=True)
-        self.update_events_thread.start()
-
-    """
-    Update events thread
-    """
-    def __update_events(self, events = []):
         self.thread_lock.acquire()
         self.active_events = list(events)
         self.thread_lock.release()
 
     """
-    Start the updating loop at each clock tick
-    """
-    def start(self):
-        self.update_thread = threading.Thread(target=self.__update_loop,daemon=True)
-        self.ti = time.time()
-        self.update_thread.start()
-
-    """
     Stop the running thread, set values to zero and start again
     """
-    def restart(self):
-        self.stop()
-        self.update_thread.join()
+    def reset(self):
         self.max_value = 0
         self.ti = 0
         self.active_events = []
@@ -69,7 +49,6 @@ class A2SN_RUN(A2SN_BASE):
         self.graph.node[0]['value'] = 1.0
         for node in range(1, self.node_count):
             self.graph.node[node]['value'] = 0.0
-        self.start()
 
     """
     Override plot method to make it thread safe
@@ -80,45 +59,12 @@ class A2SN_RUN(A2SN_BASE):
         self.thread_lock.release()
 
     """
-    Stop the updating threads
-    """
-    def stop(self):
-        self.thread_lock.acquire()
-        self.stop_thread = True
-        self.thread_lock.release()
-
-    """
-    Thread function that updates the node values
-    at each clock tick
-    """
-    def __update_loop(self):
-        # While not stop
-        stop = False
-        ti = time.time()
-        while not stop:
-            # update graph values
-            self.thread_lock.acquire()
-            self.__update()
-            self.thread_lock.release()
-            te = time.time() - ti
-            # check that dt < clock tick and wait/continue
-            if (A2SN_RUN.tick - te) > 0:
-                time.sleep(A2SN_RUN.tick - te)
-            # thread safe loop ending
-            self.thread_lock.acquire()
-            if self.stop_thread:
-                stop = True
-            self.thread_lock.release()
-            # time it
-            ti = time.time()
-    """
     Update the node values
     """
-    def __update(self):
+    def update(self):
+        self.thread_lock.acquire()
         self.max_value = 0
         messages = {}
-        if (len(self.active_events) < 1) and (self.graph.node[0]['value'] == 1):
-            return
         # for each active event
         for event in self.active_events:
             # check triggered edges -> add node input messages
@@ -133,3 +79,4 @@ class A2SN_RUN(A2SN_BASE):
                     self.graph.node[node]['value'] += sum(messages[node])
                 self.graph.node[node]['value'] *= A2SN_RUN.decay_factor
                 self.max_value = max(self.max_value, self.graph.node[node]['value']/self.graph.node[node]['depth'])
+        self.thread_lock.release()
--- python/main.py
+++ python/main.py
@@ -2,60 +2,127 @@ from A2SN_BUILD import *
 from A2SN_RUN import *
 from kitchen_objects import *
 from sequence_generator import *
-import matplotlib.pyplot as plt
 import random as rn
 import time
-
+import matplotlib.pyplot as plt
 import threading
 import time
 
 
 class PlotPoolA2SN:
-    def __init__(self, pool, dt):
+    def __init__(self, pool, tick):
         self.pool = pool
-        self.dt = dt
+        self.tick = tick
         self.nfigs = 0
-        self.stop_thread = False
-        self.thread_lock = threading.Lock()
-        self.plot_thread = threading.Thread(target=self.__plot_loop,daemon=True)
-        self.plot_thread.start()
+        self.stop_loop = False
 
     def __plot_init(self):
+        plt.ion()
         self.nfigs = 0
         for A2SN in self.pool.values():
             self.nfigs += 1
             A2SN.plot(self.nfigs)
 
-    def __plot_loop(self):
-        plt.ion()
-        self.__plot_init()
-        stop = False
-        t0 = time.time()
-        while not stop:
-            for A2SN in self.pool.values():
-                A2SN.plot()
-            plt.show()
-            plt.pause(0.0001)
-            dt = time.time() - t0
-            if (self.dt - dt) > 0:
-                time.sleep(self.dt - dt)
-            self.thread_lock.acquire()
-            if self.stop_thread:
-                stop = True
-            self.thread_lock.release()
-            t0 = time.time()
+    def __plot_end(self):
         plt.ioff()
         plt.show()
         plt.close('all')
 
+    def __plot_update(self):
+        for A2SN in self.pool.values():
+            A2SN.plot()
+        plt.show()
+        plt.pause(0.0001)
+
+    def plot_loop(self):
+        self.stop_loop = False
+        self.__plot_init()
+        t0 = time.time()
+        while not self.stop_loop:
+            self.__plot_update()
+            dt = time.time() - t0
+            if (self.tick - dt) > 0:
+                time.sleep(self.tick - dt)
+            else:
+                print("WARNING - plot loop slower than desired rate: " + str(dt))
+            t0 = time.time()
+        self.__plot_end()
+
     """
-    Stop the plot thread and remove all figures
+    Stop the plot loop and remove all figures
     """
     def stop(self):
+        self.stop_loop = True
+
+class RunPoolA2SN:
+    def __init__(self, pool, tick):
+        self.pool = pool
+        self.tick = tick
+        self.detected = False
+        self.stop_update = False
+        self.update_thread = 0
+        self.thread_lock = threading.Lock()
+        self.ti = 0
+
+    def __update_loop(self):
+        while not self.__check_stop():
+            # update graph values
+            for A2SN in self.pool.values():
+                A2SN.update()
+            # check action recognition
+            self.__check_detection()
+            dt = time.time() - self.ti
+            # check that dt < clock tick and wait/continue
+            if (self.tick - dt) > 0:
+                time.sleep(self.tick - dt)
+            else:
+                print("WARNING - update loop slower than desired rate: " + str(dt))
+            # time it
+            self.ti = time.time()
+
+    def __check_stop(self):
+        # thread safe loop ending check
         self.thread_lock.acquire()
-        self.stop_thread = True
+        check = self.stop_update
         self.thread_lock.release()
-        self.plot_thread.join()
+        return check
+
+    def __check_detection(self):
+        if not self.detected:
+            maxvals = [(A2SN.graph.node[0]['action'], A2SN.max_value) for A2SN in self.pool.values()]
+            relvals =[(maxvals[i][0], [(maxvals[i][1] - maxvals[j][1]) >  maxvals[j][1] for j in range(len(maxvals)) if j != i]) for i in range(len(maxvals))]
+            th = 10
+            for i in range(len(maxvals)):
+                if maxvals[i][1] > th:
+                    superior = True
+                    for j in relvals[i][0]:
+                        if not j:
+                            superior = False
+                    if superior:
+                        val = maxvals[i][1]
+                        mval = max([maxvals[k][1] for k in range(len(maxvals)) if k != i])
+                        self.detected = True
+                        print("DETECTED :" + maxvals[i][0] + " at t = " + str(0) + " conf: " + str(val/mval))
+                        break
+
+    def stop_update_loop(self):
+        self.thread_lock.acquire()
+        self.stop_update = True
+        self.thread_lock.release()
+        self.update_thread.join()
+
+    def start_update_loop(self):
+        self.detected = False
+        for A2SN in self.pool.values():
+            A2SN.reset()
+        self.update_thread = threading.Thread(target=self.__update_loop,daemon=True)
+        self.ti = time.time()
+        self.update_thread.start()
+
+    def restart_update_loop(self):
+        self.stop_update_loop()
+        self.start_update_loop()
+
 
 o = KitchenObjects()
 
@@ -130,64 +197,30 @@ for action in MasterSequences.keys():
 
 ##################
 # Test the program
-plotter = PlotPoolA2SN(A2SN_pool,0.1)
-for i in range(10):
+plotter = PlotPoolA2SN(A2SN_pool,0.5)
+updater = RunPoolA2SN(A2SN_pool, 0.2)
+#sequenceSim = SequenceSimulator(A2SN_pool, MasterSequences)
+
+updater.start_update_loop()
+for A2SN in A2SN_pool.values():
+    A2SN.update_events([11])
+plotter.plot_loop()
+
+"""
+for i in range(1):
     # 1 - generate sequence
     action = rn.choice(list(MasterSequences.keys()))
     sequence_generator = rn.choice(MasterSequences[action])
     seq = sequence_generator.generate()
-    # 2 - start A2SNs
-    for A2SN in A2SN_pool.values():
-        if i == 0:
-            A2SN.start()
-        else:
-            A2SN.restart()
-    # 3 - run sequence
+    # 2 - run sequence
     print(action)
     ti = time.time()
-    found = False
-    timefound = 0
-    tottime = 0
     for new_event, all_events, dt, te in seq:
         print("******************************** " + str(new_event))
         for A2SN in A2SN_pool.values():
             A2SN.update_events(all_events)
-
-        maxvals = [(A2SN.graph.node[0]['action'], A2SN.max_value) for A2SN in A2SN_pool.values()]
-        relvals =[(maxvals[i][0], [(maxvals[i][1] - maxvals[j][1]) >  maxvals[j][1] for j in range(len(maxvals)) if j != i]) for i in range(len(maxvals))]
-        th = 10
-        tottime += dt
-        if not found:
-            for i in range(len(maxvals)):
-                if maxvals[i][1] > th:
-                    superior = True
-                    for j in relvals[i][0]:
-                        if not j:
-                            superior = False
-                    if superior:
-                        found = True
-                        timefound = te
-                        val = maxvals[i][1]
-                        mval = max([maxvals[k][1] for k in range(len(maxvals)) if k != i])
-                        print("DETECTED :" + maxvals[i][0] + " at t = " + str(timefound) + " conf: " + str(val/mval))
-                        print(val)
-                        print(mval)
-                        print(maxvals)
-                        break
-
         timeel = time.time() - ti
         if (dt - timeel) > 0:
             time.sleep(dt - timeel)
-
         ti = time.time()
-
-    print("EDT: " + str(timefound/tottime))
-
-#plt.ioff()
-#plt.show()
-
-for A2SN in A2SN_pool.values():
-    A2SN.stop()
-
-while True:
-    pass
+"""
